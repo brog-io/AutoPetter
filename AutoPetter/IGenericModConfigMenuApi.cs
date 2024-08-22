11942cf09d7fd9163d4b3cf7ac8711cabb9017d6
Brogio <contact@brog.io>
2024-08-22 18:19:37 +0200

    Added Generic Mod Config Menu support

---
+++ AutoPetter/IGenericModConfigMenuApi.cs
@@ -0,0 +1,8 @@
+﻿using StardewModdingAPI;
+
+public interface IGenericModConfigMenuApi
+{
+    void Register(IManifest mod, Action reset, Action save, bool titleScreenOnly = false);
+    void AddBoolOption(IManifest mod, Func<bool> getValue, Action<bool> setValue, Func<string> name, Func<string> tooltip = null, string fieldId = null);
+
+}
--- AutoPetter/ModEntry.cs
+++ AutoPetter/ModEntry.cs
@@ -18,24 +18,53 @@ namespace AutoPetMod
 
     public class ModEntry : Mod
     {
-        private ModConfig? _config;
+        private ModConfig _config = null!;
 
         public override void Entry(IModHelper helper)
         {
             _config = helper.ReadConfig<ModConfig>();
-            if (_config == null)
-            {
-                _config = new ModConfig();
-                helper.WriteConfig(_config);
-            }
+
             helper.Events.GameLoop.DayStarted += OnDayStarted;
+            helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+        }
+
+        private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+        {
+            // Get Generic Mod Config Menu's API (if it's installed)
+            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+                return;
+
+            // Register mod
+            configMenu.Register(
+                mod: ModManifest,
+                reset: () => _config = new ModConfig(),
+                save: () => Helper.WriteConfig(_config)
+            );
+
+            // Add some config options
+            configMenu.AddBoolOption(
+                mod: ModManifest,
+                name: () => "Pet Animals",
+                tooltip: () => "Whether to automatically pet farm animals",
+                getValue: () => _config.PetAnimals,
+                setValue: value => _config.PetAnimals = value
+            );
+
+            configMenu.AddBoolOption(
+                mod: ModManifest,
+                name: () => "Pet Pets",
+                tooltip: () => "Whether to automatically pet your pets (cats/dogs)",
+                getValue: () => _config.PetPets,
+                setValue: value => _config.PetPets = value
+            );
         }
 
         private void OnDayStarted(object? sender, DayStartedEventArgs e)
         {
-            if (_config?.PetAnimals ?? true)
+            if (_config.PetAnimals)
                 PetAllAnimals();
-            if (_config?.PetPets ?? true)
+            if (_config.PetPets)
                 PetAllPets();
         }
 
